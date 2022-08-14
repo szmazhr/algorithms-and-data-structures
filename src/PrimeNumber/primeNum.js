d04d3f944c916c9545426be190da86e3845b5faa
Shahzar Mazhar <mohdshahzar.1996@gmail.com>
2022-08-15 03:10:56 +0530

    Added isPrime using iteration

---
+++ src/PrimeNumber/primeNum.js
@@ -0,0 +1,31 @@
+/**
+ * This function takes a number and returns true if the given number is prime number otherwise returns false.
+ * @param {number} n - The number to check if it is prime.
+ * @returns {boolean} - True if the number is prime otherwise false.
+ * @example
+ * isPrime(0); // true
+ * isPrime(1); // false
+ * isPrime(2); // true
+ * isPrime(3); // true
+ * isPrime(5); // true
+ * isPrime(7); // true
+ * isPrime(43); // true
+ * isPrime(46); // false
+ * isPrime(100); // false
+ * isPrime(101); // true;
+ */
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i += 1) /* O(n) */ {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+module.exports = isPrime;
+
+// Big-O: O(n)
