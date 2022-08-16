c4495db1d20628b83bed665a94c318a0feb0c70f
Shahzar Mazhar <mohdshahzar.1996@gmail.com>
2022-08-17 00:09:53 +0530

    Implemented Linked list in Stack

---
+++ src/DataStructure/LinkedList/LinkedListStack.js
@@ -0,0 +1,33 @@
+const LinkedList = require('./LinkedList');
+
+class LinkedListStack {
+  constructor() {
+    this.list = new LinkedList();
+  }
+
+  push(value) {
+    this.list.prepend(value);
+  }
+
+  pop() {
+    return this.list.removeFromBeginning();
+  }
+
+  peek() {
+    return this.list.head.data;
+  }
+
+  isEmpty() {
+    return this.list.isEmpty();
+  }
+
+  getSize() {
+    return this.list.getSize();
+  }
+
+  print() {
+    return this.list.print();
+  }
+}
+
+module.exports = LinkedListStack;
---
+++ src/DataStructure/LinkedList/LinkedListStack.test.js
@@ -0,0 +1,51 @@
+const LinkedListStack = require('./LinkedListStack');
+
+describe('linkedListStack', () => {
+  let linkedListStack;
+  beforeEach(() => {
+    linkedListStack = new LinkedListStack();
+  });
+
+  test('can be instantiated', () => {
+    expect(linkedListStack).toBeDefined();
+  });
+
+  test('can push and pop', () => {
+    linkedListStack.push(1);
+    linkedListStack.push(2);
+    linkedListStack.push(3);
+    expect(linkedListStack.pop()).toBe(3);
+    expect(linkedListStack.pop()).toBe(2);
+    expect(linkedListStack.pop()).toBe(1);
+    expect(linkedListStack.pop()).toBe('List is empty');
+  });
+
+  test('can peek', () => {
+    linkedListStack.push(1);
+    linkedListStack.push(2);
+    linkedListStack.push(3);
+    expect(linkedListStack.peek()).toBe(3);
+    expect(linkedListStack.peek()).toBe(3);
+  });
+
+  test('can check if empty', () => {
+    expect(linkedListStack.isEmpty()).toBe(true);
+    linkedListStack.push(1);
+    expect(linkedListStack.isEmpty()).toBe(false);
+  });
+
+  test('can get size', () => {
+    expect(linkedListStack.getSize()).toBe(0);
+    linkedListStack.push(1);
+    expect(linkedListStack.getSize()).toBe(1);
+    linkedListStack.push(2);
+    expect(linkedListStack.getSize()).toBe(2);
+  });
+
+  test('can print', () => {
+    linkedListStack.push(1);
+    linkedListStack.push(2);
+    linkedListStack.push(3);
+    expect(linkedListStack.print()).toBe('3 -> 2 -> 1 -> ');
+  });
+});
